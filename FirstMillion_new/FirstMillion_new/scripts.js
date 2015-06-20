c0c0b71303cf8e65791d3d625e8965866ebfd04b
ZadorozhnaOlga <olzadorozhna@gmail.com>
2015-06-21 02:01:55 +0300

    Initial Commit

---
+++ FirstMillion_new/FirstMillion_new/scripts.js
@@ -0,0 +1,25 @@
+﻿//var HideMail = function(){
+    
+//    var email = document.getElementById('TxtMail');
+//    var btn = document.getElementById('BntSend');
+   
+//    email.style.cssClass = "Mail";
+//    btn.style.cssClass = "Send";
+//    var result = PageMethods.BtnSendd();
+//    return result;
+//    //email.style.cssClass = "Mail";
+//    //btn.style.cssClass = "Send";
+//}
+var SendMail = function () {
+    //HideMail();
+
+    var email = document.getElementById('TxtMail').value;
+    if (email != "")
+    {
+        alert('На пошту ' + '"' + email + ' " ' + 'надіслано запит про допомогу.');
+    }
+    
+    
+    
+   
+    };
