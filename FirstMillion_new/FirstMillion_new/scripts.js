c804452a308a50e80b1c8f385ff4454ff083fb98
ZadorozhnaOlga <olzadorozhna@gmail.com>
2015-06-23 00:40:17 +0300

    Add

---
+++ FirstMillion_new/FirstMillion_new/scripts.js
@@ -0,0 +1,27 @@
+﻿//var HideMail = function(){
+    
+//    var email = document.getElementById('TxtMail');
+//    var btn = document.getElementById('BntSend');
+   
+//    email.style.cssClass = "Mail";
+//    btn.style.cssClass = "Send";
+//    var result = PageMethods.BtnSendd();
+//    return result;
+//    //email.style.cssClass = "Mail";
+//    //btn.style.cssClass = "Send";
+//}
+var SendMail = function () {
+    //HideMail();
+
+    var email = document.getElementById('TxtMail').value;
+    var myemail = document.getElementById('TxtMymail').value;
+    var pass = document.getElementById('TxtPass').value;
+    if ((email != "") && (myemail != "") && (pass != ""))
+    {
+        alert('Невдовзі ваш друг отримає запит про допомогу на свою пошту ' + '"' + email + '"');
+    }
+    
+    
+    
+   
+    };
